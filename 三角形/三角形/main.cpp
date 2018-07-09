7cdc4b033432ec673b739215f942cdd1f712018c
Cstzhangzb <devzhangzb@163.com>
2018-07-09 11:09:50 +0800

    hello gl

---
+++ 三角形/三角形/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  三角形
+//
+//  Created by bigfish on 2018/7/9.
+//  Copyright © 2018年 bigfish. All rights reserved.
+//
+
+#include <iostream>
+#include <glad/glad.h>
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
